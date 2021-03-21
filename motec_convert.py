0b5657a7ba910851d7626dbdf070322ffaa0924e
PapaChain <braydenjw00@gmail.com>
2021-03-20 22:40:39 -0400

    starting on Metadata

--- motec_convert.py
+++ motec_convert.py
@@ -23,10 +23,40 @@ def write_header(outfile, unique1, unique2):
 
 
 def write_metadata(outfile, data):
-    today = datetime.date.today()
-    date = (f"{today.day}/{today.month}/{today.year}")
+    metadata = data[0]
+    laps = data[1]
+
+    # Write Date
+    date = metadata.values[6][1]
     zeros = 32 - len(date)
     outfile.write(date.encode() + b"\x00" * zeros)
+
+    # Write Time
+    time = metadata.values[7][1]
+    zeros = 32 - len(time)
+    outfile.write(time.encode() + b"\x00" * zeros)
+
+    # Write Driver
+    driver = metadata.values[3][1]
+    zeros = 64 - len(driver)
+    outfile.write(driver.encode() + b"\x00" * zeros)
+
+    # Write Car
+    car = metadata.values[2][1]
+    zeros = 128 - len(car)
+    outfile.write(car.encode() + b"\x00" * zeros)
+
+    # Write Track + lots of Zeros
+    track = metadata.values[1][1]
+    zeros = 1152 - len(track)
+    outfile.write(track.encode() + b"\x00" * zeros)
+
+    # Write Unknown
+    outfile.write(b"\x22\x02\xD2\x00\x00\x00\x54\x65\x73\x74")
+    outfile.write(b"\x00" * 60)
+
+    # Write Comment
+
 
 
 def write_telemetry_headers(outfile, data):
@@ -41,15 +71,15 @@ def parse_csv(outfile, csv_file):
     metadata = parse.read_metadata(csv_file)
     write_metadata(outfile, metadata)
     
-    data_desc = parse.get_telemetry_headers(csv)
-    write_telemetry_headers(outfile, data_desc)
+    data_desc = parse.get_telemetry_headers(csv_file) # Does Nothing
+    #write_telemetry_headers(outfile, data_desc)
     
-    data = parse.parse_telemetry(csv)
-    write_telemetry(outfile, csv)
+    data = parse.read_telemetry(csv_file) 
+    #write_telemetry(outfile, csv)
 
 
 def test_write_header():
-    outfile = open("out/test.ld", "wb")
+    outfile = open("out/test.ld", "wa")
     write_header(outfile, "garabage", "garabage")
     write_metadata(outfile)
 
@@ -68,7 +98,11 @@ def test_read_csv_header():
 def main():
     start = time.perf_counter()
     
-    test_read_csv_header()
+    outfile = open("out/test.ld", "wb")
+    csv = "data/07-2020-Liberator-Session1.csv"
+    
+    write_header(outfile, "garbage", "garbage")
+    parse_csv(outfile, csv)
     # test_write_header()
     
     end = time.perf_counter()
